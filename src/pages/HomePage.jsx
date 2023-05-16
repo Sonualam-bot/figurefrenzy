085978763d98c1652451db24367f1da56d2dc521
Sonu <sonualam1599@gmail.com>
2023-05-16 10:54:31 +0530

    Style : added flex property to the hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,23 @@ import heroSection from "../images/heroSection.png"
 export const HomePage = () => {
     return (
         <>
-            <div className="hero_section">
-                <div>
+            <div className="hero_section"></div>
+            <div className="hero_aside_content" >
+                <div className="hero_img" >
                     <img src={heroSection} alt="hero" />
                 </div>
-                <div>
+                <div className="hero_section_content" >
                     <h1>Buy your favourite character action figures here</h1>
-                    <input type="text" placeholder="Search for an action figure" />
-                    <input type="button" value="Search" />
-                </div>
+                    <div className="search&button" >
+                        <input className="hero_section_content_input" type="text" placeholder="Search for an action figure" />
+                        <button className="hero_section_content_button" >Search</button>
+                    </div>
 
+                    <p>The most complete action figures More than 1000 <br /> collectible action figure ready to collect</p>
+                </div>
             </div>
+
+
         </>
     )
 }
