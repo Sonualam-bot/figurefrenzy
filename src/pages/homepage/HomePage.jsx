8fad37c7165471c0318cf5cd7b5368b3896ecced
Sonu <sonualam1599@gmail.com>
2023-05-26 20:07:15 +0530

    Feat : added new hero img

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -5,6 +5,8 @@ import { AiFillGithub } from "react-icons/ai"
 import { AiFillTwitterCircle } from "react-icons/ai"
 import { AiFillInstagram } from "react-icons/ai"
 
+import { Spinner } from "../../loaders/Spinner";
+
 
 import { categoryContext } from "./CategoryContext";
 
@@ -28,24 +30,33 @@ export const HomePage = () => {
                 {/* hero section container */}
                 <div className="hero_section">
 
-
-                    <div className="hero_aside_content" >
-                        {/* hero section img */}
-                        <div className="hero_img" >
-                            <img src={heroSection} alt="hero" />
+                    {/* Expermenatal */}
+                    {/* <div className="main_img_div" >
+                        <div className="imgone">
+                            <div className="imgone_inner" ></div>
+                            <div className="imgone_secondInner" > </div>
                         </div>
+                        <div className="img_2" ></div>
+                        <div className="img_3" ></div>
+                        <div className="img_4" ></div>
+                    </div> */}
+                    <img src="https://cdn.shopify.com/s/files/1/0550/2337/0490/files/my-capricorn-friend-slider-home_0_1512x529.jpg?v=1638376375" alt="hero" />
 
 
-                        {/* hero section title content */}
-                        <div className="hero_section_content" >
+                    {/* <div className="hero_aside_content" > */}
+                    {/* hero section img */}
+                    {/* <div className="hero_img" >
+                            <img src={heroSection} alt="hero" />
+                        </div> */}
+
+
+                    {/* hero section title content */}
+                    {/* <div className="hero_section_content" >
                             <h1>Buy your favourite character action figures here</h1>
-                            {/* <div className="search_button" >
-                                <input className="hero_section_content_input" type="text" placeholder="Search for an action figure" />
-                                <button className="hero_section_content_button" >Search</button>
-                            </div> */}
+
                             <p>The most complete action figures More than 1000 <br /> collectible action figure ready to collect</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
 
 
